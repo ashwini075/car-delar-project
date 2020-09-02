c5a83ed6a917974208f6610ddb3aba4470a5a5d9
ashwini075 <ashwinigaikar075@gmail.com>
2020-09-02 11:59:21 +0530

    git push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [{path:'Login',component:LoginComponent},
 {path:'Navbar',component:NavbarComponent},
-{path:'footer',component:FooterComponent}];
+{path:'footer',component:FooterComponent},
+{ path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
