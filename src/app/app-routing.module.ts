caccbb19a4c5109afb034cf3961536f8844e5c06
ashwini075 <ashwinigaikar075@gmail.com>
2020-09-04 15:38:29 +0530

    images

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { HomeComponent } from './home/home.component';
 import { LogoutModalComponent } from './logout-modal/logout-modal.component';
 import { RegisterComponent } from './register/register.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 const routes: Routes = [
 {path:'login',component:LoginComponent},
@@ -18,6 +19,7 @@ const routes: Routes = [
   { path: 'logout', component:LogoutModalComponent  },
 ],},
 { path: 'register',component:RegisterComponent},
+{ path:'contact-us',component:ContactUsComponent},
 { path:'',redirectTo:'/login',pathMatch:'full'},
 { path: '**', component: PageNotFoundComponent },
 
